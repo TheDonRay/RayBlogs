87161a339e43a514719c70c1de9209170bd363aa
Rayat Chowdhury <rayatchowdhury2005@gmail.com>
2026-02-08 20:28:00 -0500

    working on intro stuff then will get into the graph ql stuff

--- backend/controllers/intro.controller.js
+++ backend/controllers/intro.controller.js
@@ -1,5 +1,9 @@
 const introController = async (req, res) => { 
-
+    // User hits a get request to what is RayBlogs  
+    //so im simply sending a response to the backend here as such 
+    res.status(200).json({ 
+        "What is Ray Blogs?": "RayBlogs is not what you think it is. I bet you were expecting a frontend for this blog site but I wanted to switch it up completely. Instead of a frontend for blog posts why not make it a backend and have people send HTTP requests to fetch the latest blog or information about me and my day to day life",
+    })
 }   
 
 module.exports = introController; 
