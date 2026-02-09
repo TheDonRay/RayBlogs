741e9cda47b9d1c7643e24833e88b9bd51850074
Rayat Chowdhury <rayatchowdhury2005@gmail.com>
2026-02-08 20:19:32 -0500

    working on the intro part here

--- backend/app.js
+++ backend/app.js
@@ -2,7 +2,10 @@ const express = require("express");
 const app = express();
 
 // set up the express json middleware here as such
-app.use(express.json());
+app.use(express.json()); 
+
+// import routes and attach a endpoint to them as such 
+
 
 app.get("/", (req, res) => {
   res.json({
