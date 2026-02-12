ca4516f9a5e6b95da3a343f60cefc9bb82926ef9
Rayat Chowdhury <rayatchowdhury2005@gmail.com>
2026-02-12 01:01:29 -0500

    working on the backend of the project

---
+++ backend/config/dbconnection.js
@@ -0,0 +1,17 @@
+const mongoose = require('mongoose');  
+const URI = process.env.URI; 
+
+// create the connection function here as such 
+const DbConnection = async () => {  
+    // split this into a try and catch case here as such 
+    try { 
+        await mongoose.connect(URI); 
+        console.log('Successfully Connected to Database');  
+        return true; 
+    } catch (error) { 
+        console.error('Error connecting to the database', error);  
+        return false; 
+    }
+}; 
+
+module.exports = DbConnection; 
