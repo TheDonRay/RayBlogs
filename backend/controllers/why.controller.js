976c2f153181647f5a90bae770967a590742b9c3
Rayat Chowdhury <rayatchowdhury2005@gmail.com>
2026-02-12 18:09:04 -0500

    working on the backend

--- backend/controllers/why.controller.js
+++ backend/controllers/why.controller.js
@@ -1,6 +1,9 @@
-const whyRayBlogs = () => { 
+const whyRayBlogs = (req, res) => { 
     const whyray = req.query; 
-    console.log('user typed in', whyray); 
+    console.log('user typed in', whyray);  
+    res.status(200).json({ 
+        WhyRay: 'RayBlogs reimagines the diary by making blogging API-driven. Instead of a traditional interface, posts are accessed and updated through HTTP requests — creating a completely different kind of blog experience.'
+    })
 }; 
 
 module.exports = whyRayBlogs; 
