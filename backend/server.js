9c3bbe938d36c3901896eddcab4473e496705d2d
Rayat Chowdhury <rayatchowdhury2005@gmail.com>
2026-02-12 17:43:36 -0500

    working on the dbconnection

--- backend/server.js
+++ backend/server.js
@@ -1,11 +1,13 @@
 require("dotenv").config();
-const app = require("./app.js");
+const app = require("./app.js"); 
+const dbConnection = require('./config/dbconnection.js');
 
 const PORT = process.env.PORT;
 
 // set up the function for the actual apollo server stuff so that you understand it fully and correctly.
 const startServer = async () => {
   // db config
+  await dbConnection(); 
   // apollo server stuff here
 
   // general app instance start for express below here
