535d245e97df2d85d33bee568e9b9d2e925fee15
Rayat Chowdhury <rayatchowdhury2005@gmail.com>
2026-02-13 20:32:04 -0500

    wrong commit data

--- backend/controllers/intro.controller.js
+++ backend/controllers/intro.controller.js
@@ -1,8 +1,10 @@
-// Start of with the first blog here 
-const blog = ` 
-  type Query ( 
-    Post Details: String, 
-    Posted Created By: String, 
-    Date Post was Created: Int
-  )
-`; 
+const introController = async (req, res) => {
+  // User hits a get request to what is RayBlogs
+  //so im simply sending a response to the backend here as such
+  res.status(200).json({
+    "What is Ray Blogs?":
+      "RayBlogs isn’t what you think it is. You might expect a traditional frontend blog, but I decided to flip the idea entirely. Instead of rendering posts visually, RayBlogs lives on the backend—where users fetch the latest entries and insights about my day-to-day life through HTTP requests.",
+  });
+};
+
+module.exports = introController;
