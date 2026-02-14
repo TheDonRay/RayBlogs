e4d478f8f2c6ceef5fe8388015423d71cfffadca
Rayat Chowdhury <rayatchowdhury2005@gmail.com>
2026-02-14 14:16:08 -0500

    working on the backend

---
+++ backend/graphql/resolvers/post.resolver.js
@@ -0,0 +1,26 @@
+//import the database here as such 
+const postdatabase = require('../../model/database.model.js'); 
+
+const postResolver = { 
+    Query: { 
+        posts: async () => { 
+            // set up a try and catch case here as such 
+            try { 
+                const foundPost = await postdatabase.find(); 
+                if (!foundPost) { 
+                    throw new Error('Error finding post from database'); 
+                } 
+                return foundPost; 
+            } catch (error) { 
+                console.error('There was an error fetching from the database', error);
+            }
+        },   
+
+        post: async () => { 
+            // searching the database for a specific type of post. 
+        }
+
+    }
+}; 
+
+module.exports = postResolver; 
