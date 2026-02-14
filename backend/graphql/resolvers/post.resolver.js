56c9034645dbeeb5ce89d6a68854cd84ded0618c
Rayat Chowdhury <rayatchowdhury2005@gmail.com>
2026-02-14 16:28:48 -0500

    working on the backend

--- backend/graphql/resolvers/post.resolver.js
+++ backend/graphql/resolvers/post.resolver.js
@@ -16,12 +16,22 @@ const postResolver = {
             }
         },   
 
-        post: async () => { 
+        post: async (id) => { 
             // searching the database for a specific type of post.  
-            // implement try and catch case here to make sure its good. 
-        }
-
-    }
+            // implement try and catch case here to make sure its good.  
+            try { 
+                const specificPost = await postdatabase.findById(id); 
+                if (!specificPost) { 
+                    throw new Error('Error in finding a specific post'); 
+                }  
+                return specificPost; 
+            } catch (error) { 
+                console.error('Error with finding post from database', error); 
+            }
+        }  
+    } 
+    
+    // now we can handle the mutations here as such which is completely different here as such 
 }; 
 
 module.exports = postResolver; 
