a52ece56e2ea6c86956ede7a42c878048f0ee9c6
Rayat Chowdhury <rayatchowdhury2005@gmail.com>
2026-02-16 18:09:11 -0500

    working on Ray blogs

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,7 @@
 require("dotenv").config();
 const app = require("./app.js");
-const dbConnection = require("./config/dbconnection.js");
+const dbConnection = require("./config/dbconnection.js"); 
+const apolloserver = require('./graphql/ApolloServer.js'); 
 
 const PORT = process.env.PORT;
 
@@ -10,12 +11,17 @@ const startServer = async () => {
   // db config
   await dbConnection();
   // apollo server stuff here
-
+  await apolloserver.start(); 
+  
+  // need to instantiate the middleware to be used here as such and mount the specific route 
+  app.use( 
+    '/api/v1/rayblogs', express.json(), expressMiddleware(apolloserver)
+  ); 
   // general app instance start for express below here
   app.listen(PORT, () => {
     console.log(`RayBlogs Running on http://localhost:${PORT}`);
   });
 };
 
-//invoke the function below here as such
+//invoke the server start function below here as such to make sure it starts. 
 startServer();
