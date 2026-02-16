d1995ffe33ae8f751521a5426b0ba4f53580b280
Rayat Chowdhury <rayatchowdhury2005@gmail.com>
2026-02-16 18:06:00 -0500

    working on the backend ofrayblogs

--- backend/graphql/ApolloServer.js
+++ backend/graphql/ApolloServer.js
@@ -3,6 +3,14 @@ const { ApolloServer } = require("@apollo/server");
 // get all the typeDefinitions here as such
 const { blogPosts } = require("../graphql/typeDefs");
 // get all the resolvers here as such
-const {};
+const {postQueryResolver, postMutation } = require('../graphql/resolvers'); 
 
 // export the apollo server here as such.
+const apolloserver = new ApolloServer({ 
+    //define the type definitions and the resolvers here as such 
+    typeDefs: [blogPosts], 
+    resolvers: [postQueryResolver, postMutation], 
+}); 
+
+// export the server instance here as such 
+module.exports = apolloserver; 
